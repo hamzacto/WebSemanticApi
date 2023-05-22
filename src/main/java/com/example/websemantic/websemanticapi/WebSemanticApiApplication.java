c4e8d54ec7d2eaa53dbcd2499d1c978af21e8055
Hamzaecto <hamzaelmkadem20@gmail.com>
2023-05-22 10:19:02 +0200

    Initial commit

---
+++ src/main/java/com/example/websemantic/websemanticapi/WebSemanticApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.websemantic.websemanticapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSemanticApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSemanticApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websemantic/websemanticapi/WebSemanticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websemantic.websemanticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSemanticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
